1363f54669b04d0a5697d6f17584423c0450324b
nakao <nakaosensei@gmail.com>
2016-08-28 17:54:32 -0300

    Estruturas carregadas, filtros iniciais feitos

---
+++ src/Filter.jl
@@ -0,0 +1,55 @@
+module FilterModule
+	function removerPontos(texto::AbstractString)
+		return replace(texto, ".", "")
+	end
+	
+	function removerVirgulas(texto::AbstractString)
+		return replace(texto, ",","")	
+	end
+ 
+	function removerParenteses(texto::AbstractString)
+		texto = replace(texto, "(","")
+		return replace(texto, ")","")
+	end
+
+	function removerColchetes(texto::AbstractString)
+		texto = replace(texto, "[","")
+		return replace(texto,"]","")
+	end	
+
+	function removerNumeros(texto::AbstractString)
+		texto = replace(texto,"0","")		
+		texto = replace(texto,"1","")
+		texto = replace(texto,"2","")
+		texto = replace(texto,"3","")
+		texto = replace(texto,"4","")
+		texto = replace(texto,"5","")
+		texto = replace(texto,"6","")
+		texto = replace(texto,"7","")
+		texto = replace(texto,"8","")
+		return replace(texto,"9","")		
+	end
+
+	toUpperCase(texto::AbstractString) = uppercase(texto)
+		
+	function removeEmptyObjects(v::Array{SubString{UTF8String},1})
+		palavras=AbstractString[]		
+		for s in v
+			if(length(s)>0)
+				push!(palavras,s)
+			end
+		end
+		return palavras
+	end
+	
+	function filtrar(texto::AbstractString)
+		texto=removerPontos(texto)
+		texto=removerVirgulas(texto)
+		texto = removerParenteses(texto)
+		texto = removerColchetes(texto)
+		texto = removerNumeros(texto)
+		texto=toUpperCase(texto)
+		texto = replace(texto,"\n","")			
+		return texto
+	end	
+end
